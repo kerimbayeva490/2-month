8a5c7d5c3d95c19203d547bd504156f95a702728
kerimbayeva490 <kerimbayevam490@gmail.com>
2024-05-03 16:21:22 +0600

    hw8

--- config.py
+++ config.py
@@ -1,20 +1,29 @@
 from aiogram import Bot, Dispatcher, types
 from dotenv import load_dotenv
 from os import getenv
-from pathlib import Path
 from db.database import Database
-
+from pathlib import Path
 
 load_dotenv()
-bot = Bot(token=getenv('BOT_TOKEN'))
+dev = getenv('DEV')
+if not bool(dev):
+        from aiogram.client.session.aiohttp import AiohttpSession
+
+        print("Production ready")
+        session = AiohttpSession(proxy=getenv('PROXY'))
+        bot = Bot(token=getenv('BOT_TOKEN'), session=session)
+else:
+        print('admin')
+        bot = Bot(token=getenv('BOT_TOKEN'))
 dp = Dispatcher()
-database = Database(
-    Path(__file__).parent / "db.sqlite"
-)
+database = Database(Path('__file__').parent / 'db.sqlite')
+
 
 async def set_my_menu():
-    await bot.set_my_commands([
-        types.BotCommand(command="start", description="Начало"),
-        types.BotCommand(command="random_picture", description="Показать картинку"),
-        types.BotCommand(command="menu", description="Магазин"),
-    ])
+    await bot.set_my_commands(
+        [
+            types.BotCommand(command='start', description='начальное меню'),
+            types.BotCommand(command='menu', description='меню блюд'),
+
+        ]
+    )
